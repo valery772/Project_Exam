b5bf63793e94f43de2014dae893cdc7a3094113f
valery772 <valerinskaya9972@gmail.com>
2021-09-06 01:20:55 +0300

    Exam_Project Included all files from my project

--- target/classes/main/java/libs/TestData.java
+++
@@ -1,6 +0,0 @@
-package libs;
-
-public class TestData {
-    public final static String VALID_EMAIL="valerinskaya9972@gmail.com";
-    public final static String VALID_PASSWORD=".Cp9P4QtS5VatBk";
-}
